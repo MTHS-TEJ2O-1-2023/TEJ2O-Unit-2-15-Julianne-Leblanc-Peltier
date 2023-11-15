f2a28f35457cc8256730c6ad7f429b7d752440fd
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2023-11-15 13:28:44 -0500

    NOT finished

--- main.ts
+++ main.ts
@@ -1,8 +1,33 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2023 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Julianne Leblanc-Peltier
+ * Created on: Nov 2023
  * This program ...
 */
 
-basic.showString('Hello, World!')
+// variables
+let sprite: game.LedSprite = null
+let loopCounter = 0
+let loopCounter2 = 0
+
+// setup
+basic.showIcon(IconNames.Happy)
+basic.pause(500)
+basic.clearScreen()
+
+input.onButtonPressed(Button.A, function () {
+  basic.clearScreen()
+  loopCounter = 0
+  sprite = game.createSprite(0, 0)
+
+  while (loopCounter <= 4)
+    sprite.turn(Direction.Right, 90)
+        
+    while (loopCounter <= 5) {
+        basic.pause(500)
+        sprite.move(1)
+        loopCounter++
+    }
+  sprite.delete()
+  basic.showIcon(IconNames.Happy)
+})
