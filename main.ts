f5a85f887cbaa67e6ee4303cd6b984716ceca9f1
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2023-12-11 13:21:12 -0500

    typescript

--- main.ts
+++ main.ts
@@ -2,32 +2,43 @@
  *
  * Created by: Julianne Leblanc-Peltier
  * Created on: Nov 2023
- * This program ...
+ * This program moves the led around the microbit
 */
 
 // variables
+let ledCoordinate: number = 0
+let loopCounter: number = 0
 let sprite: game.LedSprite = null
-let loopCounter = 0
-let loopCounter2 = 0
 
 // setup
-basic.showIcon(IconNames.Happy)
-basic.pause(500)
 basic.clearScreen()
+basic.showIcon(IconNames.Happy)
 
+// when button A is pressed, move sprite around edge
 input.onButtonPressed(Button.A, function () {
+  // setup
   basic.clearScreen()
-  loopCounter = 0
   sprite = game.createSprite(0, 0)
+  loopCounter = 0
 
-  while (loopCounter <= 4)
-    sprite.turn(Direction.Right, 90)
-        
-    while (loopCounter <= 5) {
-        basic.pause(500)
-        sprite.move(1)
-        loopCounter++
+  // reset
+  while (loopCounter <= 3) {
+    ledCoordinate = 0
+
+    // move ledCoordinate
+    while (ledCoordinate <= 4) {
+      basic.pause(500)
+      sprite.move(1)
+      ledCoordinate++
     }
+
+  // turn 90 degrees
+  sprite.turn(Direction.Right, 90)
+  loopCounter++
+  }
+
+    // clean up
   sprite.delete()
+  basic.pause(500)
   basic.showIcon(IconNames.Happy)
 })
